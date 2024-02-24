e4f5ec4a03c16ef059afc838ff2ff027d7c4853e
DiegoMendozaVadai <diegoldeve@gmail.com>
2024-02-24 16:58:38 -0600

    first commit

---
+++ vite-project/src/components/Student.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="container">
+    <div class="row justify-content-center">
+      <div class="col-md-10">
+        <h3 class="text-center text-dark mt-2">CRUD Laravel Vue3</h3>
+        
+      </div>
+    </div>
+  </div>
+</template>
